6614c91a12871b5a14e264c1bd3137e73b7c0823
EgorOsaulenko <egorkapomidorka4000@gmail.com>
2024-10-30 21:39:14 +0200

    1

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-from flask import Flask
-
-from routers.pizza import pizza_route
-from models.pizza import Pizza
-from models.ingredient import Ingredient
-from models.base import create_db
-
-
-app = Flask(__name__)
-app.register_blueprint(pizza_route)
+from app import main
 
 
 if __name__ == "__main__":
-    create_db()
-    app.run(debug=True)
+    main()
