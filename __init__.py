af0b0864874e317eb8a68eeb88f681b9405e6b8e
EgorOsaulenko <egorkapomidorka4000@gmail.com>
2024-11-02 11:15:34 +0200

    1

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from app.db import create_db
 from app.routes import position_route, user_route, post_route
+from app.routers.pizza import pizza_route
+from app.routes.main import main_route 
 from app.data.password import ADMIN_PASS
 
 app = Flask(__name__)
@@ -8,6 +10,8 @@ app.secret_key = ADMIN_PASS
 app.register_blueprint(position_route)
 app.register_blueprint(user_route)
 app.register_blueprint(post_route)
+app.register_blueprint(pizza_route)
+app.register_blueprint(main_route) 
 
 def main():
     create_db()
--- app/routes/post.py
+++ app/routes/post.py
@@ -1,22 +1,28 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
-from app.db.models.post import Post
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from app.db import Session
+from app.db.models import User, Post
 from app.data.password import ADMIN_PASS
 
 post_route = Blueprint("posts", __name__)
 
-@post_route.route("/", methods=["GET", "POST"])
+@post_route.route("/posts/", methods=["GET", "POST"])
 def add_post():
     with Session() as session:
+        users = session.query(User).all() 
+
         if request.method == "POST":
             title = request.form.get("title")
             text = request.form.get("text")
             user_id = request.form.get("user_id")
-            if request.form.get("password") == ADMIN_PASS:
+            password = request.form.get("password")
+
+            if password == ADMIN_PASS:
                 post = Post(title=title, text=text, user_id=user_id)
                 session.add(post)
                 session.commit()
                 flash("Статтю успішно додано")
+                return redirect(url_for("posts.index"))
             else:
                 flash("Невірний пароль. Доступ заблоковано")
-    return render_template("add_post.html")
+
+    return render_template("add_post.html", users=users)
